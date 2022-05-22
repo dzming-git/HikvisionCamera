387b8b2423fb508123e2f3580de25a3f517d07c2
dzming-git <dzm_work@163.com>
2022-05-22 17:39:24 +0800

    添加测试文件

--- user/test.cpp
+++ user/test.cpp
@@ -1,1 +1,16 @@
 #include "hikvision_cpp.h"
+int main()
+{
+	HikCamera camera;
+	camera.init();
+	camera.login("192.168.1.16", "admin", "scuimage508");
+	camera.getImgInit();
+	Mat img;
+	while (1)
+	{
+		camera.getImg(img, false);
+		imshow("test", img);
+		waitKey(1);
+	}
+	return 0;
+}
