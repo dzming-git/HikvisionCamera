88176febfbc46ea4b913055b3df92e462512f351
dzming-git <dzm_work@163.com>
2022-07-04 16:27:35 +0800

    完善测试代码

--- user/test.cpp
+++ user/test.cpp
@@ -1,31 +1,29 @@
 #include "hikvision_cpp.h"
-#include <opencv2\core\core.hpp>
-#include <opencv2\highgui\highgui.hpp>
-#include <opencv2\imgproc\imgproc.hpp>
+#include <core\core.hpp>
+#include <highgui\highgui.hpp>
+#include <imgproc\imgproc.hpp>
 #include <iostream>
 using namespace cv;
 using namespace std;
 int main()
 {
-	HikCamera camera1;
-	HikCamera camera2;
-	cout << HikCamera::init();
-	cout << camera1.login("192.168.1.16", "admin", "scuimage508");
-	cout << camera1.getImgInit();
-	cout << camera2.login("192.168.1.7", "admin", "scuimage508");
-	cout << camera2.getImgInit();
-	Mat img1, img2;
-	unsigned char* p_buf1 = nullptr;
-	unsigned char* p_buf2 = nullptr;
-	while (1)
+	string cameraIp = "192.168.1.16";
+	HikCamera camera;
+	HikCamera::init();
+	camera.login(cameraIp.data(), "admin", "scuimage508");
+	camera.getImgInit();
+
+	Mat imgYUV420, imgBGR;
+	unsigned char* p_buf = new unsigned char[(camera.nHeight + camera.nHeight / 2) * camera.nWidth];
+	while (1) 
 	{
-		camera1.getImgBuf(p_buf1);
-		img1 = Mat(camera1.nHeight, camera1.nWidth, CV_8UC3, p_buf1);
-		imshow("test1", img1);
-		waitKey(1);
-		camera2.getImgBuf(p_buf2);
-		img2 = Mat(camera2.nHeight, camera2.nWidth, CV_8UC3, p_buf2);
-		imshow("test2", img2);
+		if (false == camera.getYUV420Buf(p_buf)) 
+		{
+			continue;
+		}
+		imgYUV420 = Mat(camera.nHeight + camera.nHeight / 2, camera.nWidth, CV_8UC1, p_buf);
+		cvtColor(imgYUV420, imgBGR, COLOR_YUV2BGR_YV12);
+		imshow(cameraIp.data(), imgBGR);
 		waitKey(1);
 	}
 	return 0;
